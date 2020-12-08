41c4d520d6cc09bc3c495eebce267716da677792
Artur <arturjataide@gmail.com>
2020-12-08 21:12:37 +0100

    log challenge

--- src/functions/challenge.js
+++ src/functions/challenge.js
@@ -1,5 +1,6 @@
 exports.handler = async (event) => {
-    console.log('EVENT ', event);
+    console.log('EVENT Body', event.body);
+    console.log('EVENT Challange', event.body.challenge);
     return {
         statusCode: 200,
         body: event.body.challenge,
